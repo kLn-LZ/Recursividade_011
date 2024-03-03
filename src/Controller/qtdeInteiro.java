64732538068dc9cc5d65cd4f503f8167bbdd3a1f
kLn-LZ <160866564+kLn-LZ@users.noreply.github.com>
2024-03-03 15:52:19 -0300

    Comentários adicionados

--- src/Controller/qtdeInteiro.java
+++ src/Controller/qtdeInteiro.java
@@ -9,13 +9,14 @@ public class qtdeInteiro {
 
     public int funcqtdeInteiro (int n ) {
 
+        // Checando se o valor chegou no mínimo de dígitos possível
         if (n / 10 <= 0) {
 
             return 1;
         }
 
 
-
+        // Somando mais 1 (dígito) e diminuindo uma casa do valor;
         return 1 + funcqtdeInteiro(n /10);
 
     }
